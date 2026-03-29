dc82c51f4c5ab0d1e8aa82882c7e8f8ae19c9610
RaviRajBhardwaj <exploringwith.ravirajbhar@gmail.com>
2026-03-29 20:39:32 +0530

    chore: add inpector for debug [use Alt + x]

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,12 @@ const config = {
 			return isExternalLibrary ? undefined : true;
 		}
 	},
-	kit: { adapter: adapter() }
+	kit: { adapter: adapter() },
+	vitePlugin: {
+		dynamicCompileOptions: ({ filename }) =>
+			filename.includes('node_modules') ? undefined : { runes: true },
+		inspector: true
+	}
 };
 
 export default config;
