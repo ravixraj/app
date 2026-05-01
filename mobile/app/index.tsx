2a35f03ac43c742290079bade4651233508b754a
RaviRajBhardwaj <exploringwith.ravirajbhar@gmail.com>
2026-05-01 19:15:08 +0530

    init: setup expo app

---
+++ mobile/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import "../global.css";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 items-center justify-center"
+    >
+      <Text className="text-red-500">Home Screen.</Text>
+    </View>
+  );
+}
